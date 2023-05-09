e888d022d25aabc6e5ca9421656863f19b3f078e
yinon11 <yinon11@gmail.com>
2023-05-09 23:50:15 +0300

    removed security for GET request to cancel and show job. Those requests are anyway using a long request id which can act as authenticator

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -40,6 +42,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests(authorize -> authorize
+                        .requestMatchers(new AntPathRequestMatcher("/requests/get_list", HttpMethod.GET.name())).permitAll() // allow access to GET /requests/get_list
+                        .requestMatchers(new AntPathRequestMatcher("/requests/cancel_task", HttpMethod.GET.name())).permitAll() // allow access to GET /requests/cancel_task)
                         .anyRequest().authenticated()
                 )
 //                .authorizeRequests(authorize -> authorize
